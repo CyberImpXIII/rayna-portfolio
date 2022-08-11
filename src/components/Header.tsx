c4cc94d575cd5eb2a99922a96957aa04dd964c8d
CyberImpXIII <jnelsenepstein@gmail.com>
2022-08-11 02:38:57 -0400

    created a seperate folder for non route components

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+const Header = ()=>(
+    <header className="App-header">
+    <div className='wideblock black title'>RW
+      </div>
+    <div className='headerrainbow'></div>
+  </header>
+)
+export default Header
---
+++ src/components/Nav.tsx
@@ -0,0 +1,17 @@
+const Nav = ()=>(
+    <nav className='navigation'>
+    <div className='navrainbows'>
+      <div className='leftnavrainbow'>
+      </div>
+      <div className='rightnavrainbow'>
+      </div>
+    </div>
+    <div className='navoptions'>
+      {/* {props.navoptions.map((option)=>(
+        <a href={option.destination}><div style={{backgroundColor:option.color}} className='option title'>{option.title}</div></a>
+      ))} */}
+    </div>
+  </nav>
+)
+
+export default Nav
