84c5a56bdc6e61fb676669d33c1c0a0da8af5883
CyberImpXIII <jnelsenepstein@gmail.com>
2023-09-11 01:53:36 -0400

    hopefully resolved race condition for ipad

--- src/components/DownArrow.tsx
+++ src/components/DownArrow.tsx
@@ -1,7 +1,7 @@
 import '../styles/DownArrow.css'
 
-const DownArrow = (props:{setDropDownActive?:Function, dropDownActive?:boolean})=>(
-<div onClick={()=>{props.setDropDownActive && props.setDropDownActive(!props.dropDownActive)}} className='downArrowContainer'>
+const DownArrow = ({setDropDownActive, dropDownActive}:{setDropDownActive?:Function, dropDownActive?:boolean})=>(
+<div onClick={()=>{setDropDownActive && dropDownActive === true && setDropDownActive(!dropDownActive)}} className='downArrowContainer'>
     <svg className='downArrow' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
         viewBox="0 0 330 330" xmlSpace="preserve">
     <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
@@ -38,7 +38,7 @@ const DownArrow = (props:{setDropDownActive?:Function, dropDownActive?:boolean})
     <g>
     </g>
     </svg>
-    <svg className='downArrow bottomArrow' style={props.dropDownActive?{opacity:1}:{opacity:0}} version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+    <svg className='downArrow bottomArrow' style={dropDownActive?{opacity:1}:{opacity:0}} version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
         viewBox="0 0 330 330" xmlSpace="preserve">
     <path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
         c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
